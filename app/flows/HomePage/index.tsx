18150a818346235b1672266822355c882550a14d
Alessandro Scarpa <alessandro.scarpa@spindox.it>
2025-10-15 10:53:44 +0200

    fix: add process changes for insert and delete operations

--- app/flows/HomePage/index.tsx
+++ app/flows/HomePage/index.tsx
@@ -4,7 +4,7 @@ import {
   FlowerAction,
   useFlowerForm,
 } from '@flowerforce/flower-react'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { EventsList } from '@/components/EventsList'
 import { withBackgroundImage } from '@/hocs/withLayout'
 import { GetEvents } from '@/components/Actions/GetEvents'
@@ -13,23 +13,45 @@ import { Event } from '@/model/Events'
 
 export const HomePage = withBackgroundImage(() => {
   const { getData, setData } = useFlowerForm({ flowName: 'homePage' })
-  useEffect(() => {
-    const onClose = watchAllEventsChanges((change: any) => {
-      const { fullDocument } = change
 
-      const prevEvents = getData('events') ?? []
+  const processChanges = useCallback(
+    (change: any) => {
+      const { fullDocument, operationType, documentKey } = change
 
-      const updatedEvents = prevEvents.map((event: Event) =>
+      const prevEvents: Event[] = getData('events') ?? []
+
+      if (operationType === 'insert') {
+        setData(
+          [...prevEvents, { ...fullDocument, id: fullDocument._id.toString() }],
+          'events',
+        )
+        return
+      }
+
+      if (operationType === 'delete') {
+        setData(
+          prevEvents.filter(({ id }) => id !== documentKey._id.toString()),
+          'events',
+        )
+        return
+      }
+
+      const updatedEvents = prevEvents.map((event) =>
         event.id === fullDocument._id.toString()
           ? { ...event, status: fullDocument.status }
           : event,
       )
       setData(updatedEvents, 'events')
-    })
+    },
+    [getData, setData],
+  )
+  useEffect(() => {
+    const onClose = watchAllEventsChanges(processChanges)
     return () => {
       onClose.then((callback) => callback())
     }
-  }, [getData, setData])
+  }, [processChanges])
+
   return (
     <Flower name="homePage">
       {/**
